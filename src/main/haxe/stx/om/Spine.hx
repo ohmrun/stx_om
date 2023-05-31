7a62dc7d9d8cc51a44425ff2527215b5aa2876ff
0b1kn00b <polysemantic@gmail.com>
2023-05-31 15:29:38 +0200

    fix: Gave Lense it's own repo

--- src/main/haxe/stx/om/Spine.hx
+++ src/main/haxe/stx/om/Spine.hx
@@ -2,7 +2,6 @@ package stx.om;
 
 @:noCompletion typedef SpineSum<T = stx.pico.Nada>  = stx.om.spine.Spine.SpineSum<T>;
 @:noCompletion typedef Spine<T = stx.pico.Nada>     = stx.om.spine.Spine.Spine<T>;
-@:noCompletion typedef Lense<T>                     = stx.om.spine.Lense<T>;
 @:noCompletion typedef SpineComparableTagCtr        = stx.om.spine.SpineComparableTagCtr;
 @:noCompletion typedef SpineOrdTagCtr               = stx.om.spine.SpineOrdTagCtr;
 @:noCompletion typedef SpineEqTagCtr                = stx.om.spine.SpineEqTagCtr;
